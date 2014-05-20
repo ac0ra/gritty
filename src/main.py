6907a4efe31cc71a8a7eae003b1f5d15e286bd07
Adam Grigolato <adam.grigolato@ezidebit.com.au>
2014-05-20 16:00:45 +1000

    Adding basic Git server template

--- src/main.py
+++ src/main.py
@@ -6,4 +6,18 @@ Author = 'Adam Grigolato'
 Version = '0'
 ###
 ###
-###
+# IMPORTS #
+from dulwich.repo import Repo
+from dulwich.server import DictBackend, TGitServer
+import threading
+
+
+
+
+repo = Repo.init("remote", mkdir=True)
+cid = repo.do_commit("message", commiter="test <test@test>")
+backend = DictBackend({'/': repo})
+dul_server = TCPGitServer(backend, 'localhost', 0)
+threading.Thread(target=dul_Server.serve).start()
+server_address, server_port = dul_server.socket.getsockname()
+
